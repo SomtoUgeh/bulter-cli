7dea803e3c1b43741bbc079b80221bc2ae9d4400
Somto M.Ugeh <smugeh@gmail.com>
2020-02-16 23:15:53 +0100

    Updates to app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 const inquirer = require("inquirer");
 const { writeFileSync } = require("fs");
 
+const jsConfig = require("./config/jsconfig.json");
 const eslintConfig = require("./config/.eslintrc.json");
 const prettierConfig = require("./config/.prettierrc.json");
-const jsConfig = require("./config/jsconfig.json");
+const tsReactConfig = require("./config/tsconfig.react.json");
+const tsReactNativeConfig = require("./config/tsconfig.react-native.json");
 
 const prettierIgnoreContent = `
 dist
@@ -18,9 +20,9 @@ docs
   const { framework } = await inquirer.prompt([
     {
       type: "list",
-      message: "Pick the framework you are using: ",
       name: "framework",
-      choices: ["react", "react-native"]
+      message: "Pick the language you are using: ",
+      choices: ["javaScript", "ts-react", "ts-react-native"]
     }
   ]);
 
@@ -28,8 +30,18 @@ docs
 
   writeFileSync(`${cwd}/.eslintrc`, JSON.stringify(eslintConfig, null, 2));
   writeFileSync(`${cwd}/.prettierrc`, JSON.stringify(prettierConfig, null, 2));
-  writeFileSync(`${cwd}/jsconfig.json`, JSON.stringify(jsConfig, null, 2));
   writeFileSync(`${cwd}/.prettierignore`, prettierIgnoreContent);
+
+  if (framework === "ts-react") {
+    writeFileSync(`${cwd}/tsconfig.react.json`, JSON.stringify(tsReactConfig, null, 2));
+  } else if (framework === "ts-react-native") {
+    writeFileSync(
+      `${cwd}/tsconfig.react-native.json`,
+      JSON.stringify(tsReactNativeConfig, null, 2)
+    );
+  } else {
+    writeFileSync(`${cwd}/jsconfig.json`, JSON.stringify(jsConfig, null, 2));
+  }
 
   console.log(`${framework} defaults successfully created`);
 })();
